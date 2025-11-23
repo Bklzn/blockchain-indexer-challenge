dfb333b2fff3870082f8bf56d67560293cadc082
Bklzn <adrian_bon@wp.pl>
2025-11-23 20:09:13 +0100

    add save block query

--- src/blocks.ts
+++ src/blocks.ts
@@ -8,6 +8,41 @@ export default async function blocksRoutes(fastify: FastifyInstance) {
   const pool = new Pool({
     connectionString: `postgres://${DB_USER}:${DB_PASSWD}@${DB_HOST}/${DB_NAME}`,
   });
+  const saveBlock = async (block: Block, errCallback: (err: any) => void) => {
+    await pool.query("BEGIN");
+    await pool
+      .query("INSERT INTO blocks (id, height) VALUES ($1, $2)", [
+        block.id,
+        block.height,
+      ])
+      .catch(errCallback);
+    for (const tx of block.transactions) {
+      await pool
+        .query("INSERT INTO transactions (id, blockid) VALUES ($1, $2)", [
+          tx.id,
+          block.id,
+        ])
+        .catch(errCallback);
+      for (let i = 0; i < tx.outputs.length; i++) {
+        await pool
+          .query(
+            "INSERT INTO outputs (txid, index, address, value) VALUES ($1, $2, $3, $4)",
+            [tx.id, i, tx.outputs[i].address, tx.outputs[i].value]
+          )
+          .catch(errCallback);
+      }
+      for (const input of tx.inputs) {
+        await pool
+          .query(
+            "INSERT INTO inputs (txid, prevtxid, previndex) VALUES ($1, $2, $3)",
+            [tx.id, input.txId, input.index]
+          )
+          .catch(errCallback);
+      }
+    }
+    await pool.query("COMMIT").catch(errCallback);
+    return true;
+  };
   fastify.post<{ Body: Block }>("/blocks", async (request, reply) => {
     const block = request.body;
     if (!block) {
@@ -31,6 +66,15 @@ export default async function blocksRoutes(fastify: FastifyInstance) {
       return reply.status(400).send({ error: "Invalid block" });
     }
 
-    return reply.status(200).send({ status: "ok" });
+    try {
+      await saveBlock(block, (err) => {
+        reply.status(400).send({ error: "Invalid query" });
+        throw new Error(err);
+      });
+      return reply.status(200).send({ message: "Block saved" });
+    } catch (err) {
+      console.error(err);
+      return reply.status(400).send({ error: "Invalid block" });
+    }
   });
 }
